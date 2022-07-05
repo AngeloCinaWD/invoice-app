b94418011d52c0df533a683edc037dc6e23b8d74
AngeloCinaWD <angelo.cina@yahoo.it>
2022-07-05 23:00:51 +0200

    retrieve data from Firestore DB

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div v-if="invoicesLoaded">
     <div v-if="!mobile" class="app flex flex-column">
       <navigation></navigation>
       <div class="app-content flex flex-column">
@@ -18,7 +18,7 @@
 </template>
 
 <script>
-import { mapState } from 'vuex';
+import { mapState, mapActions } from 'vuex';
 import InvoiceModel from './components/InvoiceModel.vue';
 import Navigation from './components/Navigation.vue';
 import Modal from './components/Modal.vue';
@@ -35,10 +35,13 @@ export default {
     };
   },
   created() {
+    this.GET_INVOICES();
     this.checkScreen();
     window.addEventListener('resize', this.checkScreen);
   },
   methods: {
+    ...mapActions(['GET_INVOICES']),
+
     checkScreen() {
       const windowWidth = window.innerWidth;
 
@@ -51,7 +54,7 @@ export default {
     },
   },
   computed: {
-    ...mapState(['invoiceModal', 'modalActive']),
+    ...mapState(['invoiceModal', 'modalActive', 'invoicesLoaded']),
   },
 };
 </script>
