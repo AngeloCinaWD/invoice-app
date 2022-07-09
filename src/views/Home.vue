49062f3a476af5487766636ab4799a23db91fc3c
AngeloCinaWD <angelo.cina@yahoo.it>
2022-07-09 19:43:22 +0200

    filtering invoices by status

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,26 @@
 <template>
   <div class="home container">
-    <!-- HEADER -->
+    <!-- Header -->
     <div class="header flex">
       <div class="left flex flex-column">
         <h1>Invoices</h1>
-        <span>There are 4 total invoices</span>
+        <span>There are {{ invoiceData.length }} total invoices</span>
       </div>
       <div class="right flex">
         <div @click="toggleFilterMenu" class="filter flex">
-          <span>Filter by status</span>
+          <span
+            >Filter by status
+            <span v-if="filteredInvoice">: {{ filteredInvoice }}</span></span
+          >
           <img src="@/assets/icon-arrow-down.svg" alt="" />
           <ul v-show="filterMenu" class="filter-menu">
-            <li>Draft</li>
-            <li>Pending</li>
-            <li>Paid</li>
-            <li>Clear Filter</li>
+            <li @click="filteredInvoices">Draft</li>
+            <li @click="filteredInvoices">Pending</li>
+            <li @click="filteredInvoices">Paid</li>
+            <li @click="filteredInvoices">Clear Filter</li>
           </ul>
         </div>
-        <div @click="newInvoice" class="class button flex">
+        <div @click="newInvoice" class="button flex">
           <div class="inner-button flex">
             <img src="@/assets/icon-plus.svg" alt="" />
           </div>
@@ -25,13 +28,13 @@
         </div>
       </div>
     </div>
-    <!-- INVOICES -->
+    <!-- Invoices -->
     <div v-if="invoiceData.length > 0">
       <Invoice
-        v-for="(invoice, index) in invoiceData"
+        v-for="(invoice, index) in filteredData"
         :invoice="invoice"
         :key="index"
-      ></Invoice>
+      />
     </div>
     <div v-else class="empty flex flex-column">
       <img src="@/assets/illustration-empty.svg" alt="" />
@@ -44,31 +47,51 @@
 </template>
 
 <script>
-import Invoice from '../components/Invoice.vue';
+import Invoice from '../components/Invoice';
 import { mapMutations, mapState } from 'vuex';
 export default {
   name: 'Home',
-  components: {
-    Invoice,
-  },
   data() {
     return {
       filterMenu: null,
+      filteredInvoice: null,
     };
+  },
+  components: {
+    Invoice,
   },
   methods: {
     ...mapMutations(['TOGGLE_INVOICE']),
-
     newInvoice() {
       this.TOGGLE_INVOICE();
     },
-
     toggleFilterMenu() {
       this.filterMenu = !this.filterMenu;
+    },
+    filteredInvoices(e) {
+      if (e.target.innerText === 'Clear Filter') {
+        this.filteredInvoice = null;
+        return;
+      }
+      this.filteredInvoice = e.target.innerText;
     },
   },
   computed: {
     ...mapState(['invoiceData']),
+    filteredData() {
+      return this.invoiceData.filter(invoice => {
+        if (this.filteredInvoice === 'Draft') {
+          return invoice.invoiceDraft === true;
+        }
+        if (this.filteredInvoice === 'Pending') {
+          return invoice.invoicePending === true;
+        }
+        if (this.filteredInvoice === 'Paid') {
+          return invoice.invoicePaid === true;
+        }
+        return invoice;
+      });
+    },
   },
 };
 </script>
